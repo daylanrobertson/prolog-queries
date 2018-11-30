212b6276e238e34c8da24baa7482d6c99b317f2a
Daylan Robertson <daylanrobertson@gmail.com>
2018-11-29 16:37:26 -0800

    example movie functionality

--- samples.pl
+++ samples.pl
@@ -12,8 +12,7 @@
 % Justin Bieber
 :- rdf_load('http://www.wikidata.org/wiki/Special:EntityData/Q34086.rdf').
 
-% actor = Q33999
-translation(actor, 'https://schema.org/actor').
+:- rdf_load('http://www.wikidata.org/wiki/Special:EntityData/Q163872.rdf').
 
 
 % Finds all objects that match the pred 'Part Of' for a given name
@@ -28,11 +27,53 @@ id(N,I) :-
     rdf(I, 'http://schema.org/name', literal(lang(en,N))).
 
 
+properties(N, P, O) :-
+    id(N, I),
+    rdf(I, P, O).
+
 % Sample query to find the mother of P, where P is full name.
 % Try mother('Justin Bieber', M).
 mother(P, N) :- 
     rdf(S, 'http://schema.org/name', literal(lang(en,P))),
-    rdf(S, 'http://www.wikidata.org/prop/direct/P25',M),
+    translation(mother, X),
+    rdf(S, X, M),
     rdf(M,'http://schema.org/name',literal(lang(en,N))).
 
+% Try sibling('Justin Bieber', M).
+sibling(P, N) :- 
+    rdf(S, 'http://schema.org/name', literal(lang(en,P))),
+    translation(sibling, X),
+    rdf(S, X, M),
+    rdf(M,'http://schema.org/name',literal(lang(en,N))).
+
+check(N, P, O) :-
+    id(N, I),
+    translation(P, X),
+    rdf(I, X, O).
+
+
+% Specific function to grab names of actors from a movie. 
+% Try cast_list('The Dark Knight', CastMember).
+cast_list(M, C) :-
+    id(M,I),
+    translation(cast, X),
+    rdf(I, X, I2),
+    id(C, I2).
+
+
+
+
+
+% some example translations
+translation(actor, 'https://schema.org/actor').
+translation(mother, 'http://www.wikidata.org/prop/direct/P25').
+translation(sibling, 'http://www.wikidata.org/prop/direct/P3373').
+translation(title, 'http://schema.org/title').
+translation(about, 'http://schema.org/about').
+
+% Movie Specific Properties
+translation(pub_date, 'http://www.wikidata.org/prop/direct/P577').
+translation(director, 'http://www.wikidata.org/prop/direct/P57').
+translation(cast, 'http://www.wikidata.org/prop/direct/P161').
+translation(role, 'http://www.wikidata.org/prop/direct/P453').
 
